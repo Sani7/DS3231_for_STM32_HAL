45ddb490f2be833da0a5e537552fe44d81e1b50e
Sander Speetjens <sander.speetjens@gmail.com>
2022-12-22 09:58:23 +0100

    Change to a time struct
    To much getters and setters used, time struct makes it more clean

--- ds3231_for_stm32_hal.c
+++ ds3231_for_stm32_hal.c
@@ -1,13 +1,14 @@
 
 /* An STM32 HAL library written for the DS3231 real-time clock IC. */
 /* Library by @eepj www.github.com/eepj */
-#include "ds3231_for_stm32_hal.h"
+#include <stdint.h>
+#include "ds3231.h"
 #include "main.h"
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-I2C_HandleTypeDef *_ds3231_ui2c;
+static I2C_HandleTypeDef *_ds3231_ui2c;
 
 /**
  * @brief Initializes the DS3231 module. Set clock halt bit to 0 to start timing.
@@ -99,43 +100,27 @@ void DS3231_ClearAlarm2Flag(){
 }
 
 /**
- * @brief Set alarm 2 minute to match. Does not change alarm 2 matching mode.
- * @param minute Minute, 0 to 59.
+ * @brief Sets alarm 2
+ * 
+ * @param time only the hour, minute, day and Wday fields are used
  */
-void DS3231_SetAlarm2Minute(uint8_t minute){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A2_MINUTE) & 0x80;
-	uint8_t a2m2 = temp | (DS3231_EncodeBCD(minute) & 0x3f);
-	DS3231_SetRegByte(DS3231_A2_MINUTE, a2m2);
-}
+void DS3231_SetAlarm2Time(ts time)
+{
+    uint8_t temp = DS3231_GetRegByte(DS3231_A2_MINUTE) & 0x80;
+	uint8_t a2 = temp | (DS3231_EncodeBCD(time.Minute) & 0x3f);
+	DS3231_SetRegByte(DS3231_A2_MINUTE, a2);
 
-/**
- * @brief Set alarm 2 hour to match. Does not change alarm 2 matching mode.
- * @param hour Hour to match in 24h format, 0 to 23.
- */
-void DS3231_SetAlarm2Hour(uint8_t hour_24mode){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A2_HOUR) & 0x80;
-	uint8_t a2m3 = temp | (DS3231_EncodeBCD(hour_24mode) & 0x3f);
-	DS3231_SetRegByte(DS3231_A2_HOUR, a2m3);
-}
+    temp = DS3231_GetRegByte(DS3231_A2_HOUR) & 0x80;
+	a2 = temp | (DS3231_EncodeBCD(time.Hour) & 0x3f);
+	DS3231_SetRegByte(DS3231_A2_HOUR, a2);
 
-/**
- * @brief Set alarm 2 date. Alarm 2 can only be set to match either date or day. Does not change alarm 2 matching mode.
- * @param date Date, 0 to 31.
- */
-void DS3231_SetAlarm2Date(uint8_t date){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A2_DATE) & 0x80;
-	uint8_t a2m4 = temp | (DS3231_EncodeBCD(date) & 0x3f);
-	DS3231_SetRegByte(DS3231_A2_DATE, a2m4);
-}
+    temp = DS3231_GetRegByte(DS3231_A2_DATE) & 0x80;
+	a2 = temp | (DS3231_EncodeBCD(time.Day) & 0x3f);
+	DS3231_SetRegByte(DS3231_A2_DATE, a2);
 
-/**
- * @brief Set alarm 2 day. Alarm 2 can only be set to match either date or day. Does not change alarm 2 matching mode.
- * @param day Days since last Sunday, 1 to 7.
- */
-void DS3231_SetAlarm2Day(uint8_t day){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A2_DATE) & 0x80;
-	uint8_t a2m4 = temp | (0x01 << DS3231_DYDT) | (DS3231_EncodeBCD(day) & 0x3f);
-	DS3231_SetRegByte(DS3231_A2_DATE, a2m4);
+    temp = DS3231_GetRegByte(DS3231_A2_DATE) & 0x80;
+	a2 = temp | (0x01 << DS3231_DYDT) | (DS3231_EncodeBCD(time.Wday) & 0x3f);
+	DS3231_SetRegByte(DS3231_A2_DATE, a2);
 }
 
 /**
@@ -171,53 +156,31 @@ void DS3231_ClearAlarm1Flag(){
 }
 
 /**
- * @brief Set alarm 1 second to match. Does not change alarm 1 matching mode.
- * @param second Second, 0 to 59.
+ * @brief Sets alarm 1
+ * 
+ * @param time only the hour, minute, second, day and Wday fields are used
  */
-void DS3231_SetAlarm1Second(uint8_t second){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A1_SECOND) & 0x80;
-	uint8_t a1m1 = temp | (DS3231_EncodeBCD(second) & 0x3f);
-	DS3231_SetRegByte(DS3231_A1_SECOND, a1m1);
-}
+void DS3231_SetAlarm1Time(ts time)
+{
+    uint8_t temp = DS3231_GetRegByte(DS3231_A1_SECOND) & 0x80;
+	uint8_t a1 = temp | (DS3231_EncodeBCD(time.Second) & 0x3f);
+	DS3231_SetRegByte(DS3231_A1_SECOND, a1);
 
-/**
- * @brief Set alarm 1 minute to match. Does not change alarm 1 matching mode.
- * @param minute Minute, 0 to 59.
- */
-void DS3231_SetAlarm1Minute(uint8_t minute){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A1_MINUTE) & 0x80;
-	uint8_t a1m2 = temp | (DS3231_EncodeBCD(minute) & 0x3f);
-	DS3231_SetRegByte(DS3231_A1_MINUTE, a1m2);
-}
+    temp = DS3231_GetRegByte(DS3231_A1_MINUTE) & 0x80;
+	a1 = temp | (DS3231_EncodeBCD(time.Minute) & 0x3f);
+	DS3231_SetRegByte(DS3231_A1_MINUTE, a1);
 
-/**
- * @brief Set alarm 1 hour to match. Does not change alarm 1 matching mode.
- * @param hour Hour, 0 to 59.
- */
-void DS3231_SetAlarm1Hour(uint8_t hour_24mode){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A1_HOUR) & 0x80;
-	uint8_t a1m3 = temp | (DS3231_EncodeBCD(hour_24mode) & 0x3f);
-	DS3231_SetRegByte(DS3231_A1_HOUR, a1m3);
-}
+    temp = DS3231_GetRegByte(DS3231_A1_HOUR) & 0x80;
+	a1 = temp | (DS3231_EncodeBCD(time.Hour) & 0x3f);
+	DS3231_SetRegByte(DS3231_A1_HOUR, a1);
 
-/**
- * @brief Set alarm 1 date. Alarm 1 can only be set to match either date or day. Does not change alarm 1 matching mode.
- * @param date Date, 0 to 31.
- */
-void DS3231_SetAlarm1Date(uint8_t date){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A1_DATE) & 0x80;
-	uint8_t a1m4 = temp | (DS3231_EncodeBCD(date) & 0x3f);
-	DS3231_SetRegByte(DS3231_A1_DATE, a1m4);
-}
+    temp = DS3231_GetRegByte(DS3231_A1_DATE) & 0x80;
+	a1 = temp | (DS3231_EncodeBCD(time.Day) & 0x3f);
+	DS3231_SetRegByte(DS3231_A1_DATE, a1);
 
-/**
- * @brief Set alarm 1 day. Alarm 1 can only be set to match either date or day. Does not change alarm 1 matching mode.
- * @param day Days since last Sunday, 1 to 7.
- */
-void DS3231_SetAlarm1Day(uint8_t day){
-	uint8_t temp = DS3231_GetRegByte(DS3231_A1_DATE) & 0x80;
-	uint8_t a1m4 = temp | (0x01 << DS3231_DYDT) | (DS3231_EncodeBCD(day) & 0x3f);
-	DS3231_SetRegByte(DS3231_A1_DATE, a1m4);
+    temp = DS3231_GetRegByte(DS3231_A1_DATE) & 0x80;
+	a1 = temp | (0x01 << DS3231_DYDT) | (DS3231_EncodeBCD(time.Wday) & 0x3f);
+	DS3231_SetRegByte(DS3231_A1_DATE, a1);
 }
 
 /**
@@ -267,149 +230,47 @@ uint8_t DS3231_IsAlarm1Triggered(){
 uint8_t DS3231_IsAlarm2Triggered(){
 	return (DS3231_GetRegByte(DS3231_REG_STATUS) >> DS3231_A2F) & 0x01;
 }
-
 /**
- * @brief Gets the current day of week.
- * @return Days from last Sunday, 0 to 6.
+ * @brief Get the current time.
+ * 
+ * @return ts Second, Minute, Hour, Day, Wday, Month, Year fields are set.
  */
-uint8_t DS3231_GetDayOfWeek(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_DOW));
-}
-
-/**
- * @brief Gets the current day of month.
- * @return Day of month, 1 to 31.
- */
-uint8_t DS3231_GetDate(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_DATE));
-}
-
-/**
- * @brief Gets the current month.
- * @return Month, 1 to 12.
- */
-uint8_t DS3231_GetMonth(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_MONTH) & 0x7f);
-}
-
-/**
- * @brief Gets the current year.
- * @return Year, 2000 to 2199.
- */
-uint16_t DS3231_GetYear(void) {
-	uint8_t decYear = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_YEAR));
-	uint16_t century = (DS3231_GetRegByte(DS3231_REG_MONTH) >> DS3231_CENTURY) * 100 + 2000;
-	return century + decYear;
-}
-
-/**
- * @brief Gets the current hour in 24h format.
- * @return Hour in 24h format, 0 to 23.
- */
-uint8_t DS3231_GetHour(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_HOUR));
-}
-
-/**
- * @brief Gets the current minute.
- * @return Minute, 0 to 59.
- */
-uint8_t DS3231_GetMinute(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_MINUTE));
-}
-
-/**
- * @brief Gets the current second. Clock halt bit not included.
- * @return Second, 0 to 59.
- */
-uint8_t DS3231_GetSecond(void) {
-	return DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_SECOND));
-}
-
-/**
- * @brief Set the current day of week.
- * @param dayOfWeek Days since last Sunday, 1 to 7.
- */
-void DS3231_SetDayOfWeek(uint8_t dayOfWeek) {
-	DS3231_SetRegByte(DS3231_REG_DOW, DS3231_EncodeBCD(dayOfWeek));
-}
-
-/**
- * @brief Set the current day of month.
- * @param date Day of month, 1 to 31.
- */
-void DS3231_SetDate(uint8_t date) {
-	DS3231_SetRegByte(DS3231_REG_DATE, DS3231_EncodeBCD(date));
-}
-
-/**
- * @brief Set the current month.
- * @param month Month, 1 to 12.
- */
-void DS3231_SetMonth(uint8_t month) {
-	uint8_t century = DS3231_GetRegByte(DS3231_REG_MONTH) & 0x80;
-	DS3231_SetRegByte(DS3231_REG_MONTH, DS3231_EncodeBCD(month) | century);
-}
-
-/**
- * @brief Set the current year.
- * @param year Year, 2000 to 2199.
- */
-void DS3231_SetYear(uint16_t year) {
-	uint8_t century = (year / 100) % 20;
-	uint8_t monthReg = (DS3231_GetRegByte(DS3231_REG_MONTH) & 0x7f) | (century << DS3231_CENTURY);
-	DS3231_SetRegByte(DS3231_REG_MONTH, monthReg);
-	DS3231_SetRegByte(DS3231_REG_YEAR, DS3231_EncodeBCD(year % 100));
-}
-
-/**
- * @brief Set the current hour, in 24h format.
- * @param hour_24mode Hour in 24h format, 0 to 23.
- */
-void DS3231_SetHour(uint8_t hour_24mode) {
-	DS3231_SetRegByte(DS3231_REG_HOUR, DS3231_EncodeBCD(hour_24mode & 0x3f));
-}
-
-/**
- * @brief Set the current minute.
- * @param minute Minute, 0 to 59.
- */
-void DS3231_SetMinute(uint8_t minute) {
-	DS3231_SetRegByte(DS3231_REG_MINUTE, DS3231_EncodeBCD(minute));
-}
-
-/**
- * @brief Set the current second.
- * @param second Second, 0 to 59.
- */
-void DS3231_SetSecond(uint8_t second) {
-	DS3231_SetRegByte(DS3231_REG_SECOND, DS3231_EncodeBCD(second));
+ts DS3231_GetTime(void)
+{
+    uint8_t decYear;
+    uint16_t century;
+    ts time;
+    time.Second = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_SECOND));
+    time.Minute = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_MINUTE));
+    time.Hour = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_HOUR) & 0x3f);
+    time.Day = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_DATE));
+    time.Wday = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_DOW));
+    time.Month = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_MONTH) & 0x7f);
+    decYear = DS3231_DecodeBCD(DS3231_GetRegByte(DS3231_REG_YEAR));
+	century = (DS3231_GetRegByte(DS3231_REG_MONTH) >> DS3231_CENTURY) * 100 + 2000;
+    time.Year = decYear + century;
+    return time;
 }
 
 /**
  * @brief Set the current time.
- * @param hour_24mode Hour in 24h format, 0 to 23.
- * @param minute  Minute, 0 to 59.
- * @param second Second, 0 to 59.
+ * 
+ * @param time Second, Minute, Hour, Day, Wday, Month, Year fields are used.
  */
-void DS3231_SetFullTime(uint8_t  hour_24mode, uint8_t minute, uint8_t second){
-	DS3231_SetHour(hour_24mode);
-	DS3231_SetMinute(minute);
-	DS3231_SetSecond(second);
-}
-
-/**
- * @brief Set the current date, month, day of week and year.
- * @param date Date, 0 to 31.
- * @param month Month, 1 to 12.
- * @param dow Days since last Sunday, 1 to 7.
- * @param year Year, 2000 to 2199.
- */
-void DS3231_SetFullDate(uint8_t date, uint8_t month, uint8_t dow, uint16_t year){
-	DS3231_SetDate(date);
-	DS3231_SetMonth(month);
-	DS3231_SetDayOfWeek(dow);
-	DS3231_SetYear(year);
+void DS3231_SetTime(ts time)
+{
+    uint8_t century;
+    uint8_t monthReg;
+    DS3231_SetRegByte(DS3231_REG_SECOND, DS3231_EncodeBCD(time.Second));
+    DS3231_SetRegByte(DS3231_REG_MINUTE, DS3231_EncodeBCD(time.Minute));
+    DS3231_SetRegByte(DS3231_REG_HOUR, DS3231_EncodeBCD(time.Hour));
+    DS3231_SetRegByte(DS3231_REG_DATE, DS3231_EncodeBCD(time.Day));
+    DS3231_SetRegByte(DS3231_REG_DOW, DS3231_EncodeBCD(time.Wday));
+    DS3231_SetRegByte(DS3231_REG_MONTH, DS3231_EncodeBCD(time.Month));
+    century = ((time.Year + 1970) / 100) % 20;
+	monthReg = (DS3231_GetRegByte(DS3231_REG_MONTH) & 0x7f) | (century << DS3231_CENTURY);
+	DS3231_SetRegByte(DS3231_REG_MONTH, monthReg);
+	DS3231_SetRegByte(DS3231_REG_YEAR, DS3231_EncodeBCD((time.Year + 1970) % 100));
 }
 
 /**
--- ds3231_for_stm32_hal.h
+++ ds3231_for_stm32_hal.h
@@ -1,6 +1,7 @@
 
 /* An STM32 HAL library written for the DS3231 real-time clock IC. */
-/* Library by @eepj www.github.com/eepj */
+/* Based on a Library by @eepj www.github.com/eepj */
+/* Moddifications by Sani7 */
 #ifndef DS3231_FOR_STM32_HAL_H
 #define DS3231_FOR_STM32_HAL_H
 #include "main.h"
@@ -73,36 +74,26 @@ typedef enum D3231_Alarm2Mode{
 	DS3231_A2_EVERY_M = 0x07, DS3231_A2_MATCH_M = 0x06, DS3231_A2_MATCH_M_H = 0x04, DS3231_A2_MATCH_M_H_DATE = 0x00, DS3231_A2_MATCH_M_H_DAY = 0x80,
 }DS3231_Alarm2Mode;
 
+typedef struct {
+    uint8_t Second;         /* seconds */
+    uint8_t Minute;         /* minutes */
+    uint8_t Hour;           /* hours */
+    uint8_t Day;            /* day of the month */
+    uint8_t Month;          /* month */
+    int16_t Year;           /* year offset from 1970 */
+    uint8_t Wday;           /* day of the week */
+    uint8_t Yday;           /* day in the year */
+    uint8_t IsDST;          /* daylight saving time */
+
+    uint32_t unixtime;      /* seconds since 01.01.1970 00:00:00 UTC*/
+} ts;
+
 extern I2C_HandleTypeDef *_ds3231_ui2c;
 
 void DS3231_Init(I2C_HandleTypeDef *hi2c);
 
 void DS3231_SetRegByte(uint8_t regAddr, uint8_t val);
 uint8_t DS3231_GetRegByte(uint8_t regAddr);
-
-uint8_t DS3231_GetDayOfWeek(void);
-uint8_t DS3231_GetDate(void);
-uint8_t DS3231_GetMonth(void);
-uint16_t DS3231_GetYear(void);
-
-uint8_t DS3231_GetHour(void);
-uint8_t DS3231_GetMinute(void);
-uint8_t DS3231_GetSecond(void);
-
-void DS3231_SetDayOfWeek(uint8_t dow);
-void DS3231_SetDate(uint8_t date);
-void DS3231_SetMonth(uint8_t month);
-void DS3231_SetYear(uint16_t year);
-
-void DS3231_SetHour(uint8_t hour_24mode);
-void DS3231_SetMinute(uint8_t minute);
-void DS3231_SetSecond(uint8_t second);
-
-void DS3231_SetFullTime(uint8_t hour_24mode, uint8_t minute, uint8_t second);
-void DS3231_SetFullDate(uint8_t date, uint8_t month, uint8_t dow, uint16_t year);
-
-uint8_t DS3231_DecodeBCD(uint8_t bin);
-uint8_t DS3231_EncodeBCD(uint8_t dec);
 
 void DS3231_EnableBatterySquareWave(DS3231_State enable);
 void DS3231_SetInterruptMode(DS3231_InterruptMode mode);
@@ -112,26 +103,26 @@ void DS3231_EnableOscillator(DS3231_State enable);
 void DS3231_EnableAlarm2(DS3231_State enable);
 void DS3231_SetAlarm2Mode(DS3231_Alarm2Mode alarmMode);
 void DS3231_ClearAlarm2Flag();
-void DS3231_SetAlarm2Minute(uint8_t minute);
-void DS3231_SetAlarm2Hour(uint8_t hour_24mode);
-void DS3231_SetAlarm2Date(uint8_t date);
-void DS3231_SetAlarm2Day(uint8_t day);
+void DS3231_SetAlarm2Time(ts time);
 
 void DS3231_EnableAlarm1(DS3231_State enable);
 void DS3231_SetAlarm1Mode(DS3231_Alarm1Mode alarmMode);
 void DS3231_ClearAlarm1Flag();
-void DS3231_SetAlarm1Second(uint8_t second);
-void DS3231_SetAlarm1Minute(uint8_t minute);
-void DS3231_SetAlarm1Hour(uint8_t hour_24mode);
-void DS3231_SetAlarm1Date(uint8_t date);
-void DS3231_SetAlarm1Day(uint8_t day);
-
-void DS3231_Enable32kHzOutput(DS3231_State enable);
+void DS3231_SetAlarm1Time(ts time);
 
 uint8_t DS3231_IsOscillatorStopped();
 uint8_t DS3231_Is32kHzEnabled();
 uint8_t DS3231_IsAlarm1Triggered();
 uint8_t DS3231_IsAlarm2Triggered();
+
+ts DS3231_GetTime(void);
+
+void DS3231_SetTime(ts time);
+
+uint8_t DS3231_DecodeBCD(uint8_t bin);
+uint8_t DS3231_EncodeBCD(uint8_t dec);
+
+void DS3231_Enable32kHzOutput(DS3231_State enable);
 
 int8_t DS3231_GetTemperatureInteger();
 uint8_t DS3231_GetTemperatureFraction();
